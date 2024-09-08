da6c32580ccc0a27ba299ed381401fda18161a30
Kanishk Krishnan <aaaaa.bbbb.ccc.dd.e.dd.ccc.bbbb.aaaaa@gmail.com>
2024-09-08 20:05:10 +0530

    joining threads

---
+++ thread_join.c
@@ -0,0 +1,26 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void* myTurn(void *arg){
+    for(int i=0;i<8;i++){
+        sleep(1);
+        printf("My Turn! %d\n",i);
+    }
+    return NULL;
+}
+
+void yourTurn(){
+    for(int i=0;i<3;i++){
+        sleep(2);
+        printf("Your Turn! %d\n",i);
+    }
+}
+
+int main(){
+    pthread_t newthread;
+
+    pthread_create(&newthread,NULL,myTurn,NULL);
+    yourTurn();
+    pthread_join(newthread,NULL);
+}
